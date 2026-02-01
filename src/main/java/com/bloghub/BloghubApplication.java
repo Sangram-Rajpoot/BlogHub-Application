96ac4dabf76c02a805170263f0022f32599b889a
Sangram Rajpoot <147055138+Sangram-Rajpoot@users.noreply.github.com>
2026-02-02 00:21:44 +0530

    Update BloghubApplication.java

--- src/main/java/com/bloghub/BloghubApplication.java
+++ src/main/java/com/bloghub/BloghubApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class BloghubApplication {
 
     public static void main(String[] args) {
