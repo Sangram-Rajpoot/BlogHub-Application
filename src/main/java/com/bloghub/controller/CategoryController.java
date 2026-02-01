18e81eb5edfe46c313642930829b3ced2d86e0f8
Sangram Rajpoot <147055138+Sangram-Rajpoot@users.noreply.github.com>
2026-02-02 00:20:29 +0530

    Update CategoryController.java

--- src/main/java/com/bloghub/controller/CategoryController.java
+++ src/main/java/com/bloghub/controller/CategoryController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
+    @CrossOrigin(origins = "*")
 public class CategoryController {
 
     private final CategoryService categoryService;
